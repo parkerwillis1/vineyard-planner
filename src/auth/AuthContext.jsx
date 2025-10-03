57cecf9c3d65b9cf380cd96c7d3728dbcde1e497
Parker Willis <parkerwillis13@gmail.com>
2025-10-03 16:44:29 -0500

    fixed some imports

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '@/shared/lib/supabaseClient';
 
 const AuthContext = createContext(null);
 
