8c133373802c93dab66ba0be1493f539a12849d3
Parker Willis <parkerwillis13@gmail.com>
2025-10-03 17:21:38 -0500

    fixed some imports 4.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 
 import { useAuth }        from "./auth/AuthContext";
 import PlannerShell from "@/features/planning/pages/PlannerShell";
-import DocumentationPage  from "@/shared/components/DocumentationPage";
+import DocumentationPage  from "./shared/components/DocumentationPage";
 import PlansPage          from "@/shared/components/PlansPage";
 import SignIn             from "./auth/SignIn";
 import SignUp             from "./auth/SignUp";
