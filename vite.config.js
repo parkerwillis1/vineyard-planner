1b121d17f91c4db4f1e3351a3eb93bef55b08611
Parker Willis <parkerwillis13@gmail.com>
2025-10-04 18:35:49 -0500

    fixed some imports 14.0

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(process.cwd(), 'src'),
     },
   },
+  build: {
+    outDir: 'dist'
+  }
 })
