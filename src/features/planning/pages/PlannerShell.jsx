3e1754f8655e161980e6ff41ddccb9001c081c85
Parker Willis <parkerwillis13@gmail.com>
2025-10-06 15:04:25 -0500

    updated

--- src/features/planning/pages/PlannerShell.jsx
+++ src/features/planning/pages/PlannerShell.jsx
@@ -3833,7 +3833,7 @@ const LTV = (landValue + improvementsValue) > 0
 );
 
 return (
-  <div className={`min-h-screen bg-gray-50`}>
+  <div className="min-h-screen bg-gray-50">
     {/* When embedded, we rely on SiteLayout's header. No local header. */}
     <main className="flex-grow w-full overflow-x-hidden">
 
