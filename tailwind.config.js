a289c601bcd87afba6d0480eb45e3f126e7391df
Parker Willis <parkerwillis13@gmail.com>
2025-10-06 14:00:38 -0500

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",   // <- all React source
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vine-green': {
+          50: '#f0fdf7',
+          100: '#dcfce9',
+          200: '#bbf7d6',
+          300: '#86efb8',
+          400: '#4ade92',
+          500: '#1a8a63', // Your main color
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+          900: '#14532d',
+        },
+      },
+    },
   },
   plugins: [],
-};
-
+}
